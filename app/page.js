dcf9a3ddb10c442c315af58102a6ea4a32c78fd5
JE <whddms87@gmail.com>
2023-04-20 17:44:48 +0900

    페이지 레이아웃 만들기(React 기초문법)

--- app/page.js
+++ app/page.js
@@ -124,10 +124,16 @@
 // }
 
 export default function Home() {
+  let name = "JE";
+  let link = "https://www.naver.com/";
+
   return (
     <div>
-      hi hi
-      <button>버튼</button>
+      {/* Layout 작성 */}
+      {/* <h2 className="title">애플후레시</h2> */}
+      <h2 style={{ color: "red", textAlign: "center" }}>애플후레시</h2>
+      <p className="title-sub">by dev {name}</p>
+      <a href={link}>네이버 링크</a>
     </div>
   );
 }
