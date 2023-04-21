272064f1323dfeb74fa8fe2557280ddbb2c4a3d3
JE <whddms87@gmail.com>
2023-04-21 18:08:16 +0900

    Next.js의 자동 Routing + CSS 적용

---
+++ app/list/page.js
@@ -0,0 +1,13 @@
+export default function List() {
+  return (
+    <div>
+      <h3 className="title">상품목록</h3>
+      <div className="food">
+        <h4>상품1 $40</h4>
+      </div>
+      <div className="food">
+        <h4>상품2 $50</h4>
+      </div>
+    </div>
+  );
+}
