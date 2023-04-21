e24a7535d0826081135502d3e8c38a38aa405ca5
JE <whddms87@gmail.com>
2023-04-21 20:09:26 +0900

    여러 페이지 만들기(Routing)

--- app/page.js
+++ app/page.js
@@ -1,13 +1,14 @@
+import Link from "next/link";
+
 export default function Home() {
   let name = "JE";
-  let link = "https://www.naver.com/";
 
   return (
     <div>
       {/* Layout 작성 */}
+      {/* 본문 */}
       <h2 className="title">애플후레시</h2>
       <p className="title-sub">by dev {name}</p>
-      <a href={link}>네이버 링크</a>
     </div>
   );
 }
