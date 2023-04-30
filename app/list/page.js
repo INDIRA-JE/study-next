395fb902510cea110aaf5a176881e021a43f0299
JE <whddms87@gmail.com>
2023-05-01 01:21:41 +0900

    좋아요 버튼 만들기 2

--- app/list/page.js
+++ app/list/page.js
@@ -3,29 +3,35 @@ import { useState } from "react"; // useState 사용 준비
 
 export default function List() {
   let 상품 = ["Tomatoes", "Pasta", "Coconut"];
-  let [수량, 수량변경] = useState(0);
+  let [수량, 수량변경] = useState([0, 1, 2]);
 
   return (
     <div>
       <h2 className="title">상품목록</h2>
-      {상품.map((a, i) => {
+      {상품.map((product, sequence) => {
         return (
-          <div className="food" key={i}>
-            {/* 이미지 추가 */}
-            <img src={`/food${i}.png`} className="food-img" />
-            {/* 상품 적용 : 순서대로 */}
-            <h3>{a} $40</h3>
+          // 첫번째 구분
+          <div className="food" key={sequence}>
+            {/* 두번째 구분 : 이미지 추가 */}
+            <img src={`/food${sequence}.png`} className="food-img" />
+            {/* 3번째 구분 : 상품 적용 : 순서대로 */}
+            <h3>{product} $40</h3>
+            {/* 4번째 구분 : 수량 */}
             <button
               onClick={() => {
-                수량변경(수량 - 1);
+                let copy = [...수량];
+                copy[0]--;
+                수량변경(copy);
               }}
             >
               -
             </button>
-            <span> {수량} </span>
+            <span> {수량[0]} </span>
             <button
               onClick={() => {
-                수량변경(수량 + 1);
+                let copy = [...수량];
+                copy[0]++;
+                수량변경(copy);
               }}
             >
               +
